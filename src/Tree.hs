4d59c36fb43f0d0a86d3374d531ae552b8e8f8af
deadxraver <409853@edu.itmo.ru>
2026-01-18 19:32:27 +0300

    interface

--- src/Tree.hs
+++ src/Tree.hs
@@ -1,5 +1,24 @@
 module Tree
-    ( TreeNode(..)
+    ( TreeNode(..),
+    fromList,
+    insert,
+    remove,
+    emptyTree
     ) where
 
 data TreeNode = TreeNode Char [TreeNode] | TreeHead [TreeNode] | TreeLeaf deriving (Eq, Show)
+
+emptyTree :: TreeNode
+emptyTree = TreeHead [TreeLeaf]
+
+insert :: String -> TreeNode -> TreeNode
+insert str node = node -- TODO: 
+
+remove :: String -> TreeNode -> TreeNode
+remove str node = node -- TODO: 
+
+fromList' :: [String] -> TreeNode -> TreeNode
+fromList' list node = node -- TODO:
+
+fromList :: [String] -> TreeNode
+fromList list = fromList' list emptyTree
