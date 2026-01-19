a5ef4ce24078ee5497f8c4329aae77401ada641c
deadxraver <409853@edu.itmo.ru>
2026-01-19 16:25:19 +0300

    set test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,13 @@
+import Tree
+
+list1dup :: [String]
+list1dup = ["hello", "hello"]
+list1 :: [String]
+list1 = ["hello"]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    setTest
+
+setTest :: IO ()
+setTest = print $ if fromList list1 == fromList list1dup then "Set Test passed" else error "Set Test failed"
