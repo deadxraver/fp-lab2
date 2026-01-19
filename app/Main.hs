e5558e86c2ab7391023acdf2fe8b309f6de8cb06
deadxraver <409853@edu.itmo.ru>
2026-01-19 15:45:57 +0300

    insert & fromList fully works, as a set

--- app/Main.hs
+++ app/Main.hs
@@ -8,7 +8,15 @@ someNode = TreeNode 'A' [TreeLeaf]
 headNode :: TreeNode
 headNode = TreeHead [someNode]
 
+nodeFromStr :: TreeNode
+nodeFromStr = insert "hell" emptyTree
+
 main :: IO ()
 main = do
     print headNode
     print $ toList headNode
+    print nodeFromStr
+    print $ toList nodeFromStr
+    print $ toList $ insert "hello" nodeFromStr
+    print $ toList $ fromList ["Abab", "AZaz"]
+    print $ toList $ fromList ["hello", "hello"]
--- src/Tree.hs
+++ src/Tree.hs
@@ -4,16 +4,60 @@ module Tree (
     toList,
     insert,
     remove,
+    clear,
     emptyTree,
 ) where
+
+import Data.List (find)
 
 data TreeNode = TreeNode Char [TreeNode] | TreeHead [TreeNode] | TreeLeaf deriving (Eq, Show)
 
 emptyTree :: TreeNode
 emptyTree = TreeHead [TreeLeaf]
 
+getChildren :: TreeNode -> [TreeNode]
+getChildren (TreeNode _ list) = list
+getChildren (TreeHead list) = list
+getChildren TreeLeaf = []
+
+hasLetter :: Char -> TreeNode -> Bool
+hasLetter c node = any checkChild (getChildren node)
+  where
+    checkChild (TreeNode c' _) = c' == c
+    checkChild _ = False
+
+findChild :: Char -> [TreeNode] -> Maybe TreeNode
+findChild c = find checkChild
+  where
+    checkChild (TreeNode c' _) = c' == c
+    checkChild _ = False
+
+isLeaf :: TreeNode -> Bool
+isLeaf TreeLeaf = True
+isLeaf _ = False
+
+replaceChild :: TreeNode -> TreeNode -> [TreeNode] -> [TreeNode]
+replaceChild old new = map replace
+  where
+    replace x = if x == old then new else x
+
 insert :: String -> TreeNode -> TreeNode
-insert str node = node -- TODO:
+insert "" (TreeHead list) = TreeHead list -- neutral element
+insert (c : str) (TreeHead list) =
+    -- insert into head
+    case findChild c list of
+        Nothing -> TreeHead (insert str (TreeNode c []) : list) -- insert new
+        Just node -> TreeHead (replaceChild node (insert str (TreeNode c (getChildren node))) list) -- replace & move forward through that node
+insert "" (TreeNode c list) =
+    -- end of string somewhere
+    if any isLeaf list
+        then TreeNode c list -- base case
+        else TreeNode c (TreeLeaf : list) -- add leaf
+insert (c : str) (TreeNode c' list) =
+    -- insert to node between head and leaf
+    case findChild c list of
+        Nothing -> TreeNode c' (insert str (TreeNode c []) : list) -- insert new
+        Just node -> TreeNode c' (replaceChild node (insert str (TreeNode c (getChildren node))) list) -- replace & move forward through that node
 
 remove :: String -> TreeNode -> TreeNode
 remove str node = node -- TODO:
@@ -34,3 +78,7 @@ toList' (TreeHead _) _ = error "TreeHead should not be here"
 toList :: TreeNode -> [String]
 toList (TreeHead []) = []
 toList (TreeHead (node : list)) = toList' node "" ++ toList (TreeHead list)
+
+clear :: TreeNode -> TreeNode
+clear (TreeHead _) = emptyTree
+clear _ = error "Passed not head"
